30e58ae495580e417c962db44eb0aec26bd92a6d
harris_trirope <harris@trirope.com>
2023-04-06 13:43:19 +0530

    web

--- lib/src/webview_view.dart
+++ lib/src/webview_view.dart
@@ -15,6 +15,7 @@ late  WebViewPlusController controller;
 
 @override
   void initState() {
+    print("init called");
     // TODO: implement initState
     super.initState();
     Future<void> loadAsset() async {
@@ -31,42 +32,36 @@ print("asserts loading from load Asset");
   @override
   Widget build(BuildContext context) {
 
-    return Column(
-              children: [
-                Container(
-                height: MediaQuery.of(context).size.height*0.90,
-                width: MediaQuery.of(context).size.width,
-                color: Colors.transparent,
-                child: WebViewPlus(
-                      javascriptMode: JavascriptMode.unrestricted,
-                      initialUrl: 'assets/index.html',
-                      onWebViewCreated: (controller) {
-                      this.controller = controller;
-                      },
-                      javascriptChannels: {
-                      JavascriptChannel(
-                      name: 'JavascriptChannel',
-                      onMessageReceived: (message) async {
-                      Map<String, dynamic> data = jsonDecode(message.message);
-                      var obj = json.encode(message.message);
-                      await showDialog(
-                          context: context,
-                          builder: (context) => AlertDialog(
-                                  content: Text(data['request'],
-                                      style:const TextStyle(fontSize: 14)),
-                                  actions: [
-                                    TextButton(
-                                        onPressed: () {
-                                          Navigator.of(context).pop();
-                                        },
-                                        child: const Text("OKAY"))
-                                  ]));
-                      controller.webViewController.evaluateJavascript('ok()');
-                    })
-          }),
-    ),
-              ],
-            );
+    return WebViewPlus(
+      onProgress: (progress) {
+        print("package Called");
+      },
+          javascriptMode: JavascriptMode.unrestricted,
+          initialUrl: 'assets/index.html',
+          onWebViewCreated: (controller) {
+          this.controller = controller;
+          },
+          javascriptChannels: {
+          JavascriptChannel(
+          name: 'JavascriptChannel',
+          onMessageReceived: (message) async {
+          Map<String, dynamic> data = jsonDecode(message.message);
+          var obj = json.encode(message.message);
+          await showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                      content: Text(data['request'],
+                          style:const TextStyle(fontSize: 14)),
+                      actions: [
+                        TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text("OKAY"))
+                      ]));
+          controller.webViewController.evaluateJavascript('ok()');
+        })
+          });
   }
 }
 
